94aa7ea88f935ec338bd0ee3b1fb8e39a0047578
Ty Augusta <taugusta06@gmail.com>
2018-07-17 11:21:42 -0700

    tm pull of search/location

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,28 +37,40 @@ var tmAPIKey = "nXFGaGABlnZ60ow3g6mS88ZUNZD5Zp5A";
 
 // Here we are building the URL we need to query the database
 var input = "";
-$("#run-search").on("click", function(event){
+$("#run-search").on("click", function (event) {
   event.preventDefault();
   var userInput = $("#search-term").val().trim();
-  console.log(userInput);
   input = userInput;
-  searchtm();
   console.log(input);
+  searchBandsInTown();
 });
 
 
-function searchtm(res){
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-  type: "GET",
-  //   url:"https://app.ticketmaster.com/discovery/v2/events.json?size=359&apikey=nXFGaGABlnZ60ow3g6mS88ZUNZD5Zp5A",
-  url: "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&keyword=" + input + "&city=phoenix&apikey=nXFGaGABlnZ60ow3g6mS88ZUNZD5Zp5A",
-  async: true,
-  dataType: "json",
-  success: function (json) {
-    console.log(json);
-    var myObj = JSON.parse(this.text);
-    console.log(myObj);
+function searchBandsInTown(artist) {
+
+  // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
+  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&keyword=" + input + "&city=phoenix&apikey=nXFGaGABlnZ60ow3g6mS88ZUNZD5Zp5A";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    // Printing the entire object to console
+    console.log(response);
+    console.log(response._embedded.events[0]._embedded.venues[0].name);
+    console.log(Object.values(response._embedded.events[0]._embedded.venues[0].location));
+
+    // Constructing HTML containing the artist information
+    var artistName = $("<h1>").text(response.name);
+    var artistURL = $("<a>").attr("href", response.url).append(artistName);
+    var artistImage = $("<img>").attr("src", response.thumb_url);
+    var trackerCount = $("<h2>").text(response.tracker_count + " fans tracking this artist");
+    var upcomingEvents = $("<h2>").text(response.upcoming_event_count + " upcoming events");
+    var goToArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
+
+    // Empty the contents of the artist-div, append the new artist content
+    $("#artist-div").empty();
+    $("#artist-div").append(artistURL, artistImage, trackerCount, upcomingEvents, goToArtist);
     // var res = json.data;
     // var resultsDiv = $("<div>");
     // console.log(res);
@@ -71,8 +83,19 @@ $.ajax({
     //   console.log(time-zone);
     // Parse the response.
     // Do other things.
-  },
-  error: function (xhr, status, err) {
-    // This time, we do not end up here!
-  }
-});};
+  });
+};
+
+
+function venueMap(maps) {
+
+  // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
+  var queryURL = "https://maps.googleapis.com/maps/api/staticmap?" + input + "&city=phoenix&apikey=nXFGaGABlnZ60ow3g6mS88ZUNZD5Zp5A";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+
+  })
+}
